c083f4981ac0733b3e121b62fea6ecc3a0f9cd7b
Harish1604 <harish16042005@gmail.com>
2026-05-06 14:57:11 +0530

    added css

--- notification_app_fe/vite.config.js
+++ notification_app_fe/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/notifications': 'http://localhost:4000',
-      '/health': 'http://localhost:4000',
+      '/notifications': 'http://127.0.0.1:4000',
+      '/health': 'http://127.0.0.1:4000',
     }
   }
 })
